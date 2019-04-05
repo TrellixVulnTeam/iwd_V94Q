186e92212c991b35e11eae6ed3697c10327eba0c
Bartlomiej Neumann <bartlomiej.neumann@siili.com>
2019-04-05 14:54:39 +0200

    Move script to its own module
    Preparatory commit for splitting single script to
    multiple files in module. This will help with further
    modifications.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 
 from setuptools import setup
 setup(name='iwd',
-      version='1.0.0-alpha01',
-      scripts=['iwd.py']
+      version='1.0.0-alpha03',
+      packages=['iwd'],
+      entry_points={
+          'console_scripts': [
+              'iwd=iwd.__main__:main'
+          ]
+      },
       )
