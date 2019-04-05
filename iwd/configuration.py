9bde189312c0975549e7b61554e8338a8e9a32fa
Bartlomiej Neumann <bartlomiej.neumann@siili.com>
2019-04-05 15:45:09 +0200

    Unify cmake argument creation
    Create helper function `make_cmake_arg` that will help
    in creating consistent cmake arguments from key,value pairs.

--- iwd/configuration.py
+++ iwd/configuration.py
@@ -6,6 +6,10 @@ import re
 ARGUMENT_REGEX = re.compile(r'\w+=[\w+/]')
 
 
+def make_cmake_arg(key, value):
+    return f'-D{key}={value}'
+
+
 class Configuration(UserDict):
     def __init__(self, initial_data):
         super().__init__(initial_data)
@@ -13,8 +17,8 @@ class Configuration(UserDict):
     def get_hash(self, hash_obj=None):
         m = hashlib.sha256() if hash_obj is None else hash_obj
         for k, v in self.data.items():
-            m.update(f'-D{k}={v}'.encode())
+            m.update(make_cmake_arg(k, v).encode())
         return m
 
     def as_cmake_args(self):
-        return [f'-D{key}={value}' for key, value in self.data.items()]
+        return [make_cmake_arg(key, value) for key, value in self.data.items()]
