682e1500264b79ba1de89f7ee42688367781e72f
Bartlomiej Neumann <bartlomiej.neumann@siili.com>
2019-04-08 21:03:51 +0200

    Remove unused method

--- iwd/configuration.py
+++ iwd/configuration.py
@@ -16,12 +16,6 @@ class Configuration(UserDict):
     def __init__(self, initial_data):
         super().__init__(initial_data)
 
-    def get_hash(self, hash_obj=None):
-        m = hashlib.sha256() if hash_obj is None else hash_obj
-        for k, v in self.data.items():
-            m.update(make_cmake_arg(k, v).encode())
-        return m
-
     def as_cmake_args(self):
         return [make_cmake_arg(key, value) for key, value in self.data.items()]
 
