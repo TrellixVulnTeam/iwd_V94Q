20525d8fadf70d18f9eb7ab18141043b1337b25c
Bartlomiej Neumann <bartlomiej.neumann@siili.com>
2019-10-12 15:23:42 +0200

    Add overload for quicktype gen. cls to jsonencoder
    When serializing the classes, detect, whether the object has to_dict
    method, and invoke it if necessary.
    
    This will allow to serialize all of quicktype classes.

--- iwd/json_encoder.py
+++ iwd/json_encoder.py
@@ -10,4 +10,6 @@ class JsonEncoder(json.JSONEncoder):
             return obj.data
         if isinstance(obj, Requirement):
             return obj.__dict__
+        if callable(getattr(obj, 'to_dict', None)):
+            return obj.to_dict()
         return json.JSONEncoder.default(self, obj)
