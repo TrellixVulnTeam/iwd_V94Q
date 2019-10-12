e827453f3a6e6f4043f517697cc45b63ab2b814f
Bartlomiej Neumann <bartlomiej.neumann@siili.com>
2019-10-12 14:49:49 +0200

    Add override_existing arg to download_file
    Adding this flag will allow user to control, whether
    the download should not care, if the file at specified path already
    exists. By default the value is set to false, as to not change the
    function behaviour when invoked with 2 args.

--- iwd/tools.py
+++ iwd/tools.py
@@ -11,8 +11,8 @@ def untargz(targzfile_path: str, output_directory: str):
         return [os.path.join(output_directory, x.name) for x in tar.getmembers()]
 
 
-def download_file(url: str, download_file_path: str):
-    if not os.path.isfile(download_file_path):
+def download_file(url: str, download_file_path: str, override_existing=False):
+    if not os.path.isfile(download_file_path) or override_existing:
         # TODO - When possible, this should print nice download status
         urllib.request.urlretrieve(
             url, filename=download_file_path)
