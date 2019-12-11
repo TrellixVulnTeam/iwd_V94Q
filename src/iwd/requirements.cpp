6763ab088589a50e62badbff9a76d466ca893d52
Bartlomiej Neumann <bartlomiej.neumann@siili.com>
2019-12-11 09:40:48 +0100

    Add name-version function
    Create function that based on the required creates
    name-version string.

--- src/iwd/requirements.cpp
+++ src/iwd/requirements.cpp
@@ -1,8 +1,15 @@
 #include "iwd/requirements.hpp"
 
 #include <vn/file.hpp>
+#include <vn/string_utils.hpp>
 
 namespace iwd {
+
+std::string
+name_version(const quicktype::Requirement& requirement)
+{
+  return vn::join("", requirement.get_name(), "-", requirement.get_version());
+}
 
 std::vector<quicktype::Requirement>
 parse_requirements(const std::filesystem::path& iwd_config_path)
--- src/iwd/requirements.hpp
+++ src/iwd/requirements.hpp
@@ -5,6 +5,9 @@
 
 namespace iwd {
 
+std::string
+name_version(const quicktype::Requirement& requirement);
+
 std::vector<quicktype::Requirement>
 parse_requirements(const std::filesystem::path& iwd_config_path);
 
