5241d9ebed985dbae7ebc85a76978686195e3ff5
Bartlomiej Neumann <bartlomiej.neumann@siili.com>
2020-01-18 15:06:27 +0100

    Add fnmatch tests
    Add tests for fnmatch, mostly taken from python.

--- test/vn/fnmatch.cpp
+++ test/vn/fnmatch.cpp
@@ -1,7 +1,22 @@
 #include <catch2/catch.hpp>
 
+#include <regex>
+#include <tuple>
 #include <utility>
 #include <vn/fnmatch.hpp>
+
+TEST_CASE("fnmatch-translate", "[fnmatch]")
+{
+  REQUIRE(vn::detail::translate_fnmatch_expr("*") == R"re(.*)re");
+  REQUIRE(vn::detail::translate_fnmatch_expr("?") == R"re(.)re");
+  REQUIRE(vn::detail::translate_fnmatch_expr("a?b*") == R"re(a.b.*)re");
+  REQUIRE(vn::detail::translate_fnmatch_expr("[abc]") == R"re([abc])re");
+  REQUIRE(vn::detail::translate_fnmatch_expr("[]]") == R"re([]])re");
+  REQUIRE(vn::detail::translate_fnmatch_expr("[!x]") == R"re([^x])re");
+  REQUIRE(vn::detail::translate_fnmatch_expr("[^x]") == R"re([\^x])re");
+  REQUIRE(vn::detail::translate_fnmatch_expr("[x") == R"re(\[x)re");
+  REQUIRE(vn::detail::translate_fnmatch_expr("ab[cd]") == R"re(ab[cd])re");
+}
 
 TEST_CASE("fnmatch", "[fnmatch]")
 {
@@ -22,11 +37,15 @@ TEST_CASE("fnmatch", "[fnmatch]")
     std::make_tuple("a/b.cpp", "a/*.cpp", true),
     std::make_tuple("a/b.cpp", "a/*.hpp", false),
     std::make_tuple("a/b/c.cpp", "a/*/c.cpp", true),
-    std::make_tuple("a/b/c/d.cpp", "a/*/d.cpp", false));
+    std::make_tuple("a/b/c/d.cpp", "a/*/d.cpp", true));
 
   vn::fnmatch match(pattern);
+  const auto translatedPattern = vn::detail::translate_fnmatch_expr(pattern);
+
   INFO(
-    "Pattern: " << std::quoted(pattern) << ", fname:" << std::quoted(fname)
+    "Pattern: " << std::quoted(pattern)
+                << ",translation:" << std::quoted(translatedPattern)
+                << ", fname:" << std::quoted(fname)
                 << ", should match:" << should_match);
   REQUIRE(match(fname) == should_match);
 }
