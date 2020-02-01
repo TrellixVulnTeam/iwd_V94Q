a487876bf30a6c794c135e869a1ac64fbdc101dc
Bartlomiej Neumann <bartlomiej.neumann@siili.com>
2020-02-01 15:42:02 +0100

    Generate iwd.cmake at the end of iwd
    The iwd.cmake is supposed to ease up consumption
    of installation targets from iwd.
    Produce it at the very end of iwd main function.

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,17 @@
 #include "iwd/git_clone.hpp"
 #include "iwd/parse_args.hpp"
 #include "iwd/requirements.hpp"
+#include <vn/file.hpp>
+#include <vn/string_template.hpp>
 #include <vn/string_utils.hpp>
 
 #include <iostream>
 
 namespace nl = nlohmann;
+
+constexpr auto iwd_cmake_file_template =
+  "set(IWD_INSTALL_PREFIX $(INSTALL_PREFIX))\n"
+  "set(CMAKE_PREFIX_PATH $(INSTALL_PREFIX))";
 
 int
 main(int argc, const char** argv)
@@ -32,6 +38,17 @@ main(int argc, const char** argv)
     handler.configure(configuration);
     handler.install();
   }
+
+  const auto iwd_cmake_contents = vn::render_template(
+    iwd_cmake_file_template,
+    [&directories](const std::string& key) -> std::optional<std::string> {
+      if (key == "INSTALL_PREFIX") {
+        return directories.install_directory().path().generic_string();
+      }
+      return std::nullopt;
+    });
+
+  vn::create_text_file(args.build_directory / "iwd.cmake", iwd_cmake_contents);
 
   return 0;
 }
