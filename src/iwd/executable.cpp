8aee4a98d77af6a13f8f7d313d71689cbcb8932b
Bartlomiej Neumann <bartlomiej.neumann@siili.com>
2020-02-01 13:29:26 +0100

    Create executable class
    Instead of calling processes directly, with assumption that
    they are installed in the users machine, create executable class
    that represents existing path to target process that can be run.
    
    Provide try_find_executable function that returns optional
    of this executable.
    
    Main idea is to call try_find_ex functions at the very begginging
    in order to ensure that all executables such as git and cmake can be
    found and launched.

---
+++ src/iwd/executable.cpp
@@ -0,0 +1,58 @@
+#include <iwd/executable.hpp>
+
+#include <boost/process.hpp>
+#include <boost/process/search_path.hpp>
+#include <vn/string_utils.hpp>
+
+namespace iwd {
+
+namespace bp = boost::process;
+
+executable::executable(const std::filesystem::path& path)
+  : _executable_path(path)
+{}
+
+void
+executable::run(
+  const std::vector<std::string>& proc_args,
+  const check_call_arguments& args) const
+{
+  check_call(_executable_path, proc_args, args);
+}
+
+std::optional<executable>
+try_find_executable(
+  const std::string& name,
+  const std::optional<std::vector<std::filesystem::path>>& extra_search_paths)
+{
+  auto search_paths = boost::this_process::path();
+  if (extra_search_paths) {
+    std::transform(
+      extra_search_paths->begin(),
+      extra_search_paths->end(),
+      std::back_inserter(search_paths),
+      [](const std::filesystem::path& p) {
+        return boost::filesystem::path(p.generic_string());
+      });
+  }
+
+  auto proc = bp::search_path(boost::filesystem::path(name), search_paths);
+  return proc.empty() ? std::optional<executable>()
+                      : std::make_optional(executable(
+                          std::filesystem::path(proc.generic_string())));
+}
+
+executable
+find_executable_or_throw(
+  const std::string& name,
+  const std::optional<std::vector<std::filesystem::path>>& extra_search_paths)
+{
+  auto result = try_find_executable(name, extra_search_paths);
+  if (!result) {
+    throw std::runtime_error(
+      vn::make_message("Could not find program named", name));
+  }
+  return result.value();
+}
+
+} // namespace iwd
---
+++ src/iwd/executable.hpp
@@ -0,0 +1,38 @@
+#ifndef IWD__EXECUTABLE_HPP
+#define IWD__EXECUTABLE_HPP
+
+#include <filesystem>
+#include <iwd/check_call.hpp>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <vn/directory.hpp>
+
+namespace iwd {
+struct executable
+{
+  explicit executable(const std::filesystem::path& path);
+  void run(
+    const std::vector<std::string>& proc_args = {},
+    const check_call_arguments& args = {}) const;
+
+private:
+  std::filesystem::path _executable_path;
+};
+
+std::optional<executable>
+try_find_executable(
+  const std::string& name,
+  const std::optional<std::vector<std::filesystem::path>>& extra_search_paths =
+    std::nullopt);
+
+executable
+find_executable_or_throw(
+  const std::string& name,
+  const std::optional<std::vector<std::filesystem::path>>& extra_search_paths =
+    std::nullopt);
+
+} // namespace iwd
+
+#endif
